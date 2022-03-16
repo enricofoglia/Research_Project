72e966c5e64f9324c0f7e99ed9a80ce23a7319ef
Enrico Foglia <enricofoglia99@gmail.com>
2022-03-16 18:32:30 +0100

    possible solution to Wagner.py. Still inefficient

--- Wagner.py
+++ Wagner.py
@@ -18,33 +18,27 @@ is the the best performing and allows for infinite domain, and quadrature).
 """
 import numpy as np
 from scipy.special import jv, yn # Bessel functions
-from scipy.integrate import quadrature, quad
+from scipy.integrate import trapezoid, quad
 
-def Wagner(t, large_times = True): 
+def Wagner(t, large_times = False): 
     if large_times == False:
         G= lambda k: (jv(1,k)*(jv(1,k)+yn(0,k))+yn(1,k)*(yn(1,k)-jv(0,k)))/((jv(1,k) + yn(0,k))**2 + (yn(1,k) - jv(0,k))**2)
-        phi = 1 + 2/np.pi*quad(lambda k: 1/k*(G(k)-1)*np.sin(k*t), 0, np.inf, limit = int(100*t))[0]
+        phi = 1/2 + 2/np.pi*quad(lambda k: 1/k*(G(k)-1/2)*np.sin(k*t), 0, 10, limit = int(100*t)+50)[0]
     else:    
         G= lambda k: (yn(1,k)*yn(0,k)+jv(1,k)*jv(0,k))/((jv(1,k) + yn(0,k))**2 + (yn(1,k) - jv(0,k))**2)
-        phi = 1 - 2/np.pi*quad(lambda k: 1/k*G(k)*np.cos(k*t), 0, np.inf, limit = int(100*t))[0]
+        phi = 1 - 2/np.pi*quad(lambda k: 1/k*G(k)*np.cos(k*t), 0, 10, limit = int(100*t))[0]
     return phi
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
-    
-    # t1 = 1e2
-    # W = Wagner(t1)
-    # print(f'Wagner function at t = {t1}: {W}')
-    # t2 = 1e2
-    # W = Wagner(t2, large_times=True)
-    # print(f'Wagner function at t = {t2}: {W}') 
+
     
     W = []
-    T = np.logspace(-1,4, num = 100)
+    T = np.logspace(-1,3, num = 100)
     for t in T:
         if t<=100:
-            W.append(Wagner(t, large_times = True))
+            W.append(Wagner(t, large_times = False))
         else:
             W.append(Wagner(t, large_times = True))
         
@@ -57,3 +51,23 @@ if __name__ == '__main__':
     ax.set_ylabel('$1-\phi (t)$')
     ax.set_title('Wagner function')
     plt.show()
+    
+    # Here a couple lines of code to see that the part of the integral that I'm
+    # cutting is not that important after all.
+    
+    # t = 1e4
+    # G= lambda k: (yn(1,k)*yn(0,k)+jv(1,k)*jv(0,k))/((jv(1,k) + yn(0,k))**2 + (yn(1,k) - jv(0,k))**2)
+    # k = np.linspace(1e-10,20,int(100*t))
+    # f = lambda k: 1/k*G(k)*np.cos(k*t)
+    # kInf = 10
+    # kSup = 100
+    # integral, error = quad(f, kInf, kSup, limit = int(100*t))
+    # print(f'The integral from k = {kInf} to k = {kSup} is:\nI = {integral} (error = {error})')    
+    # fig,ax = plt.subplots()
+    # ax.plot(k,f(k))
+    # ax.set_xlabel('k')
+    # ax.set_ylabel('f(k)')
+    # ax.grid(True)
+    
+    
+    
