5a20d3390ec840bc2b81c924f5513daaca9dd512
Enrico Foglia <enricofoglia99@gmail.com>
2022-05-31 11:19:54 +0200

    created orthogonal_polynomials.py, modified .gitignore

--- theodorsen_SINDy.py
+++ theodorsen_SINDy.py
@@ -10,6 +10,7 @@ import numpy as np
 import pysindy as ps
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp as integral
+from Theodorsen_control import *
 
 def u_fun(t):
     amplitude_alpha, amplitude_h, omega, phi = np.loadtxt('theodorsen_params.dat', dtype = float)
@@ -29,40 +30,55 @@ def u_fun(t):
                           h_dot]))
     return  u
     
+a = 1/2  # pitch axis wrt to 1/2-chord
+b = 1  # half-chord length of the airfoil
+airfoil = AirfoilGeometry(a=a, b=b)  # default values of C_1 and C_2 used
 
-theodorsen_data = np.loadtxt('theodorsen_data.dat', dtype=float)
+# the balanced truncation Theodorsen function approximation
+theodorsen_function_sys = theodorsen_function_balanced_truncation_ss()
 
-t = theodorsen_data[:,0]
-dt = t[1] - t[0]
-X = theodorsen_data[:,1]
-X = np.array([X]).T
-U = theodorsen_data[:,2:]
-U = np.reshape(U, (len(t),5))
+# generation of unsteady lift state-space systems
+
+# state-space system with both α" and h" as inputs
+theodorsen_full_sys = unsteady_lift_ss(airfoil, theodorsen_function_sys)
+# state-space system with both α" as input
+theodorsen_alpha_sys = unsteady_lift_ss(
+    airfoil, theodorsen_function_sys, inputs='alpha')
+# state-space system with both h" as input
+theodorsen_h_sys = unsteady_lift_ss(
+    airfoil, theodorsen_function_sys, inputs='h')
+
+t = np.linspace(0, 100, int(1e05))
 
 
-fig1, ax1 = plt.subplots()
+omega = 1
+amplitude = 0.1
+phase = np.pi/2
+u_alpha = amplitude*np.sin(omega*t + phase) + 0.8*amplitude*np.sin(3*omega*t + phase) + 1.2*amplitude*np.sin(4*omega*t + phase)
 
-for deg in range(2,3):
-    optimizer = ps.optimizers.stlsq.STLSQ(threshold = 0.1, alpha = 1e-05, max_iter = 100)
-    library = ps.feature_library.polynomial_library.PolynomialLibrary(degree = deg)
-    model = ps.SINDy(optimizer = optimizer, 
-   				     feature_library = library,
-   				     feature_names = ['CL', '(a)\"', '(a)\'', 'a', '(h)\"', '(h)\'']) # default paramaters:
-                                           					   						   # differentiation method: centered differences
-   
-    model.fit(X, u = U, t = dt)
-    model.print()
-    X0 = np.array([X[0]])[0]
-    model_x = model.simulate(X0, t, u = u_fun)
-    
-    end_plot =int( 5 / dt)
-    
-    ax1.plot(t[0:end_plot], model_x[0:end_plot])
-    ax1.plot(t[0:end_plot], X[0:end_plot], linestyle = '--', color = 'k')
-    ax1.set_xlabel('time tb/U')
-    ax1.set_ylabel(r'$C_L$')
-    ax1.set_title(r'Reconstructed $C_L$')
-    ax1.grid(True)
-    ax1.legend(['SINDy model', 'Original data'])
-plt.show() 
-   
+
+T = 5
+phase = T/2
+amplitude = 0.1/T
+# square wave
+u_h = np.array([amplitude if np.floor((ti+phase)/T+T/2) %
+               2 == 0 else -amplitude for ti in t])
+output = control.forced_response(
+    theodorsen_full_sys, T=t, U=np.vstack((u_h, u_alpha)))
+data_both = TheodorsenTimeResponse(output, inputs='alpha')
+data_both.phase_plot(state='alpha_e')
+
+# TODO: the pdf has to be done on the input, not the output.
+
+nb_bins = 1000
+fig, ax = plt.subplots()
+ax.hist(data_both.C_L, nb_bins, density = True)
+ax.set_xlabel(r'$C_L$')
+ax.set_ylabel(r'$f(C_L)$')
+ax.set_title(r'$C_L$ p.d.f')
+
+plt.show()
+
+
+
+
--- wagner_SINDy.py
+++ wagner_SINDy.py
@@ -124,72 +124,165 @@ def clean_data(data, tol = 2):
     plt.show()
     return data
 
-# importing data
-wagner_data = np.loadtxt('wagner_data.dat', dtype=float)
-cleaned_wagner_data = clean_data(wagner_data)
+def poly_library(X, deg = 5):
+    '''
+    Parameters
+    ----------
+    X : np.array
+        Input data.
+    deg : int, optional
+        Degree of the highest polynomial. The default is 5.
 
-t = cleaned_wagner_data[:,0] 
-x = 1 - cleaned_wagner_data[:,1] # normalized, steady-state-subtracted lift (see reference [1])
-x = np.array([x]).T
+    Returns
+    -------
+    lib : np.array
+        Polynomial library.
 
-# calculating L(t) pdf
-nb_bins = 100 # number of bins
-fig_h, ax_h = plt.subplots()
-ax_h.hist(cleaned_wagner_data[:,1], nb_bins, density = True)
-ax_h.set_xlabel('L(t)')
-ax_h.set_ylabel('p(L(t))')
-ax_h.set_title('L(t) p.d.f.')
+    '''
+    lib = X**0
+    for i in range(1,deg+1):
+    ## Classical regression
+        lib = np.concatenate((lib, X**i), axis=-1)
+    return lib
 
-fig1, ax1 = plt.subplots()
-fig2, ax2 = plt.subplots()
-fig3, ax3 = plt.subplots()
-
-for deg in range(2,9):
-	# fitting model
-    optimizer = ps.optimizers.stlsq.STLSQ(threshold = 0.1, alpha = 1e-06, max_iter = 10)
-    library = ps.feature_library.polynomial_library.PolynomialLibrary(degree = deg)
-    model = ps.SINDy(optimizer = optimizer, 
-   				     feature_library = library,
-   				     feature_names = ['phi']) # default paramaters:
-   					   						  # differentiation method: centered differences
-   
-    model.fit(x, t = t[1] - t[0])
-    model.print()
-   
-    x0 = np.array([0.5])
-    model_x = model.simulate(x0, t)
-
-    err_norm = np.abs(x - model_x) / x
-    err = np.abs(x - model_x)
-
-	# plotting
-    ax1.plot(t, model_x)
-    ax1.set_xlabel('t [s]')
-    ax1.set_ylabel(r'L(t) - $L_0$ / $L_0$')
-    ax1.set_title('Fitted Unsteady Lift')
-    ax1.set_xscale('log')
-    ax1.set_yscale('log')
-    ax1.grid(True)
-   
-    ax2.plot(t, err)
-    ax2.set_xlabel('t [s]')
-    ax2.set_ylabel(r'$L(t) - \hat{L}(t)$')
-    ax2.set_yscale('log')
-    ax2.set_title('Error')
-    ax2.grid(True)
+def fisher(X, plot = True):
+    Fisher = np.dot(np.transpose(X), X)
+    N = X.shape[1]-1
+    if plot == True:
+        fig = plt.figure(figsize=(5, 5))
+        ax = fig.add_subplot(111)
+        #plt.imshow(Fisher, cmap=cm.jet, interpolation='nearest')
+        im = ax.imshow(Fisher)
+        if N < 11:
+          for i in range(N+1):
+              for j in range(N+1):
+                  text = ax.text(j, i, "%.1f" % Fisher[i, j],
+                                ha="center", va="center", color="w")
+        plt.show()
+        return Fisher
     
-    ax3.plot(t, err_norm)
-    ax3.set_xlabel('t [s]')
-    ax3.set_ylabel(r'$\frac{L(t) - \hat{L}(t)}{1 - L(t)}$')
-    ax3.set_yscale('log')
-    ax3.set_title('Normalised Error')
-    ax3.grid(True)
+def least_squares_regression(X,Y, threshold = 0.0):
+    H = np.linalg.inv(X.T @ X) @ X.T
+    beta = H @ Y
+    if threshold != 0:
+        beta[np.abs(beta) < threshold] = 0
+    return beta
 
+if __name__ == '__main__':
+    # importing data
+    wagner_data = np.loadtxt('wagner_data.dat', dtype=float)
+    cleaned_wagner_data = clean_data(wagner_data)
+    
+    t = cleaned_wagner_data[:,0] 
+    x = 1 - cleaned_wagner_data[:,1] # normalized, steady-state-subtracted lift (see reference [1])
+    x = np.array([x]).T
+    
+    # calculating L(t) pdf
+    nb_bins = 100 # number of bins
+    fig_h, ax_h = plt.subplots()
+    ax_h.hist(cleaned_wagner_data[:,1], nb_bins, density = True)
+    ax_h.set_xlabel('L(t)')
+    ax_h.set_ylabel('p(L(t))')
+    ax_h.set_title('L(t) p.d.f.')
+    
+    fig1, ax1 = plt.subplots()
+    fig2, ax2 = plt.subplots()
+    fig3, ax3 = plt.subplots()
+    fig4, ax4 = plt.subplots()
+    
+    # csi = np.zeros((9,9))
+    
+    for deg in range(0,9):
+    	# fitting model
+        optimizer = ps.optimizers.stlsq.STLSQ(threshold = 0.1, alpha = 1e-06, max_iter = 10)
+        library = ps.feature_library.polynomial_library.PolynomialLibrary(degree = deg)
+        model = ps.SINDy(optimizer = optimizer, 
+       				     feature_library = library,
+       				     feature_names = ['phi']) # default paramaters:
+       					   						  # differentiation method: centered differences
+        
+        model.fit(x, t = t[1] - t[0])
+        #csi[0:deg+1, deg] = model.coefficients()
+        
+        model.print()
+       
+        x0 = np.array([0.5])
+        model_x = model.simulate(x0, t)
+        
+        err_norm = np.abs(x - model_x) / x
+        err = np.abs(x - model_x)
+        
+        # STATISTICS
+        fd = ps.differentiation.FiniteDifference()
+        x_prime = fd._differentiate(x, t)
+        theta = library.transform(x)
+        beta = least_squares_regression(theta, x_prime, threshold = 0.1)
+        print('Least squares:')
+        print(beta)
+        print('\n')
+        csi = model.coefficients().T
 
-ax1.plot(t, x, 'k--')
+        eps_least_squares = x_prime - theta @ beta
+        eps_SINDy = x_prime - theta @ csi
+        
+        
+    	# plotting
+        if deg > 1:
+            ax1.plot(t, model_x)
+            ax1.set_xlabel('t [s]')
+            ax1.set_ylabel(r'L(t) - $L_0$ / $L_0$')
+            ax1.set_title('Fitted Unsteady Lift')
+            ax1.set_xscale('log')
+            ax1.set_yscale('log')
+            ax1.grid(True)
+           
+            ax2.plot(t, err)
+            ax2.set_xlabel('t [s]')
+            ax2.set_ylabel(r'$L(t) - \hat{L}(t)$')
+            ax2.set_yscale('log')
+            ax2.set_title('Error')
+            ax2.grid(True)
+            
+            ax3.plot(t, err_norm)
+            ax3.set_xlabel('t [s]')
+            ax3.set_ylabel(r'$\frac{L(t) - \hat{L}(t)}{1 - L(t)}$')
+            ax3.set_yscale('log')
+            ax3.set_title('Normalised Error')
+            ax3.grid(True)
+            
+            # STATISTICS PLOT
+            if deg == 8:
+                ax4.plot(t, np.abs(eps_SINDy))
+                ax4.plot(t, np.abs(eps_least_squares))
+                ax4.set_xlabel('Time')
+                ax4.set_ylabel(r'$\varepsilon = \dot{L} - \hat{\dot{L}}$')
+                ax4.set_yscale('log')
+                
+                fig, ax = plt.subplots()
+                ax.hist(eps_SINDy, nb_bins, density = True, range = (-1e-03, 1e-03))
+                ax.set_xlabel(r'$\varepsilon$')
+                ax.set_ylabel(r'$f(\varepsilon)$')
+    
+    
+    ax1.plot(t, x, 'k--')
+    
+    ax1.legend(['r = 2','r = 3','r = 4','r = 5','r = 6','r = 7','r = 8', 'Analytical'])
+    ax2.legend(['r = 2','r = 3','r = 4','r = 5','r = 6','r = 7','r = 8'], loc = 'upper right')
+    ax3.legend(['r = 2','r = 3','r = 4','r = 5','r = 6','r = 7','r = 8'], loc = 'lower right')
+    #ax4.legend(['r = 2','r = 3','r = 4','r = 5','r = 6','r = 7','r = 8'], loc = 'upper right')
 
-ax1.legend(['r = 2','r = 3','r = 4','r = 5','r = 6','r = 7','r = 8', 'Analytical'])
-ax2.legend(['r = 2','r = 3','r = 4','r = 5','r = 6','r = 7','r = 8'], loc = 'upper right')
-ax3.legend(['r = 2','r = 3','r = 4','r = 5','r = 6','r = 7','r = 8'], loc = 'lower right')
-
-plt.show()
+    
+    plt.show()
+    
+    ## STATISTICS
+    
+    poly_lib = poly_library(x, deg = 5)
+    F = fisher(poly_lib)
+    s, v = np.linalg.eig(F)
+    
+    fig, ax = plt.subplots()
+    ax.plot(range(0,6), s/s[0], marker = 'o', linestyle = 'None')
+    ax.set_xlabel('eig number')
+    ax.set_ylabel('normalised eig magnitute')
+    ax.set_yscale('log')
+    
